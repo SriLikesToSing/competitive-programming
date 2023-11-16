b2a415866b470d33021d94ec26e4d5e5ae92d580
Sri <48895259+SriLikesToSing@users.noreply.github.com>
2023-11-16 15:55:47 -0800

    fixing broke ass solution for 125. cause I am god now.

--- cpp/leetCode/125. Valid Palindrome.py
+++ cpp/leetCode/125. Valid Palindrome.py
@@ -1,4 +1,6 @@
 class Solution:
+    
+    #broke ass solution wtf is this?
     def isPalindrome(self, s: str) -> bool:
         r = 0
         l = len(s)-1
@@ -23,6 +25,26 @@ class Solution:
             r+=1
             l-=1
             
+        return True
+
+
+    #This is what I call pretty
+    class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = "".join(x for x in s if x.isalnum())
+        s = s.lower()
+
+        if len(s) == 1 or s == "":
+            return True
+
+        front = 0
+        end = len(s)-1
+
+        while(front < len(s)//2):
+            if s[front] != s[end]:
+                return False
+            front+=1
+            end-=1
         return True
             
             
