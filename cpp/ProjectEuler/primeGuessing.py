27f874d352c56c7cb0a1c7b85a1bfd1631b06b34
Sri <48895259+SriLikesToSing@users.noreply.github.com>
2023-12-25 12:39:30 -0800

    PROJECT EULER IS SO FUCKING SICK XD

---
+++ cpp/ProjectEuler/primeGuessing.py
@@ -0,0 +1,57 @@
+'''
+
+E(N) is a function that returns the expected number of points assuming player wants to maximize thier
+score.
+
+gets a point for guessing each bit correctly
+
+E(10) = 2 because you can always guess 1 
+
+two paths:
+
+    - guess it correct
+    - whether the last bit in the number
+
+
+how tf do we maximize our with a uniform distribution?
+
+how do we guess to maximize our score?
+    - 
+
+
+https://en.wikipedia.org/wiki/Dominating_set
+
+
+'''
+
+# http://www.math-math.com/2014/01/prime-numbers-in-binary.html
+
+
+print(bin(47))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
