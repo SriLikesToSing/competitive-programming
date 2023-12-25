2eea4780c1d73514be0e174510faa3efb5c1634a
Sri <48895259+SriLikesToSing@users.noreply.github.com>
2023-12-25 12:29:56 -0800

    Boring me. Gotta do some project euler now jeez

--- cpp/leetCode/287. Find the Duplicate Number.py
+++ cpp/leetCode/287. Find the Duplicate Number.py
@@ -8,3 +8,21 @@ class Solution:
             
             dup[x] = True
 
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        s, f = 0, 0
+
+        while True:
+            s = nums[s]
+            f = nums[nums[f]]
+            if s == f:
+                break
+
+        p2 = 0
+        while True:
+            s = nums[s]
+            p2 = nums[p2]
+            if s == p2:
+                return s
+
