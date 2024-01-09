da528e20b43db70f720b9fb38a2bda11a1e7499c
Sri <48895259+SriLikesToSing@users.noreply.github.com>
2024-01-08 17:38:02 -0800

    goodNodes

---
+++ cpp/leetCode/1448. Count Good Nodes in Binary Tree.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        cnt = [0]
+
+        def bfs(root, maxx):
+            if not root:
+                return
+
+            if max(root.val, maxx) == root.val:
+                cnt[0]+=1
+            
+            bfs(root.right, max(root.val, maxx))
+            bfs(root.left, max(root.val, maxx))
+
+        bfs(root, root.val)
+        return cnt[0] 
+            
+
+
